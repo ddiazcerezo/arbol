1a06e11515973c730f81d15b7c237582ca3586ea
ddiazcerezo <ddiazcerezo@danielcastelao.org>
2014-12-14 16:42:30 +0100

    Create arbol.java

---
+++ arbol.java
@@ -0,0 +1,32 @@
+package model.arbolG;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Arbol
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Arbol(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void main() {
+		// TODO implement me	
+	}
+	
+}
